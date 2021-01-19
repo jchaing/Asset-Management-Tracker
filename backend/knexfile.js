272be7c015f6ca3241646a106feb645f7e7e64d7
john <john@spinningplates.org>
2021-01-19 13:20:49 -0800

    added db-config and updated knexfile

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,44 +1,48 @@
 // Update with your config settings.
+require('dotenv').config();
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: 'pg',
     connection: {
-      filename: './dev.sqlite3'
-    }
+      host: process.env.POSTGRES_HOST,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      charset: 'utf8',
+    },
+    migrations: { directory: './data/migrations' },
+    seeds: { directory: './data/seeds' },
+    pool: {
+      min: 2,
+      max: 10,
+    },
   },
 
   staging: {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user:     'username',
-      password: 'password'
+      user: 'username',
+      password: 'password',
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: 'knex_migrations',
+    },
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: process.env.DB_URL,
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+    migrations: { directory: './data/migrations' },
+    seeds: { directory: './data/seeds' },
+  },
 };
