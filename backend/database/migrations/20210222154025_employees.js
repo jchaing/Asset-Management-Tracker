65ddf5dad12b500acf1dfdba7fe2257140e59045
john <john@spinningplates.org>
2021-02-23 19:16:57 -0800

    added employees table

--- backend/database/migrations/20210222154025_employees.js
+++ backend/database/migrations/20210222154025_employees.js
@@ -1,18 +1,20 @@
 exports.up = function (knex) {
   return knex.schema.createTable('employees', (employees) => {
     employees.increments();
-    employees.string('email').notNullable().unique();
-    employees.string('firstName').notNullable();
-    employees.string('lastName').notNullable();
-    employees.string('dept').notNullable();
-    employees.string('manager').notNullable();
+    employees.string('email', 128).notNullable().unique();
+    employees.string('firstName', 128).notNullable();
+    employees.string('lastName', 128).notNullable();
+    employees.string('dept', 128).notNullable();
+    employees.string('manager', 128).notNullable();
     employees
-      .string('user_id')
+      .integer('user_id')
       .unsigned()
       .notNullable()
       .references('users.id')
       .onUpdate('CASCADE')
       .onDelete('CASCADE');
+    employees.string('notes', 256);
+    employees.boolean('active').notNullable().defaultTo(true);
   });
 };
 
